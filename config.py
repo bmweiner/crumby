0f5cf8131492106475ffbb91d7477f41bf042af8
bmweiner <bmweiner@users.noreply.github.com>
2017-02-05 17:08:05 -0500

    Updated settings

--- config.py
+++ config.py
@@ -3,15 +3,13 @@
 import os
 
 DOMAIN = os.environ.get('OPENSHIFT_APP_DNS')
-PKG = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR', '.'), 'crumby/')
 
 db_url = os.environ.get('OPENSHIFT_MYSQL_DB_URL', '.')
-app_name = os.environ.get('OPENSHIFT_APP_NAME', 'crumby')
-SQLALCHEMY_DATABASE_URI = os.path.join(db_url, app_name)
+SQLALCHEMY_DATABASE_URI = os.path.join(db_url, 'crumby')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-data_dir = os.environ.get('OPENSHIFT_DATA_DIR', '.')
-GEO_DB_URI = os.path.join(data_dir, 'GeoLite2-City.mmdb')
+filepath = os.environ.get('OPENSHIFT_DATA_DIR', '.')
+GEOIP2_DB_PATH = os.path.join(filepath, 'GeoLite2-City.mmdb')
 PROXY_COUNT = 0
 
 CROSSDOMAIN_ORIGIN = 'http://bmweiner.com'
