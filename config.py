debc1f9e95fa1218b0dff18d2b538ddf8899d67a
bmweiner <bmweiner@users.noreply.github.com>
2017-02-07 08:19:52 -0500

    Converted to template

--- config.py
+++ config.py
@@ -1,15 +1,7 @@
-"""Crumby prod configs."""
+"""production config."""
 
-import os
-
-DOMAIN = os.environ.get('OPENSHIFT_APP_DNS')
-
-db_url = os.environ.get('OPENSHIFT_MYSQL_DB_URL', '.')
-SQLALCHEMY_DATABASE_URI = os.path.join(db_url, 'crumby')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-filepath = os.environ.get('OPENSHIFT_DATA_DIR', '.')
-GEOIP2_DB_PATH = os.path.join(filepath, 'GeoLite2-City.mmdb')
-PROXY_COUNT = 0
-
-CROSSDOMAIN_ORIGIN = 'http://bmweiner.com'
+#DOMAIN = 'localhost:5000'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<user>:<password>@localhost/crumby'
+#GEOIP2_DB_PATH = '/path/to/data/GeoLite2-City.mmdb'
+#PROXY_COUNT = 0
+#CROSSDOMAIN_ORIGIN = '*'
