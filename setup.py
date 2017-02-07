4bf9d90e64f9fb9859c1ca09c58c101b4a1dda29
bmweiner <bmweiner@users.noreply.github.com>
2017-02-07 08:43:12 -0500

    Updated version and description

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-from crumby import __version__
-from crumby import __doc__ as description
+__version__ = '0.5'
+__description__ = 'A Flask based web analytics app'
 
 setup(name='crumby',
       version=__version__,
       author='bmweiner',
       author_email='bmweiner@users.noreply.github.com',
       url='https://github.com/bmweiner/crumby',
-      description=description,
+      description=__description__,
       license='MIT License',
-      packages=['crumby', 'crumby.utils'],
+      packages=['crumby', 'crumby.utils', 'crumby.views'],
       install_requires=['Flask', 'Flask-SQLAlchemy', 'geoip2'])
