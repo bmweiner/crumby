b2661b6cec03827d7cf5b16825cc57451f40040a
bmweiner <bmweiner@users.noreply.github.com>
2017-03-23 09:30:36 -0400

    Merge Openshift revisions.

--- crumby/views/reporting.py
+++ crumby/views/reporting.py
@@ -1,78 +1,114 @@
 """Routes for reporting."""
 
-import os
 import datetime
+import os
 from flask import request
 from flask import jsonify
+from flask import redirect
+from flask import url_for
 from flask import render_template
+from flask import flash
+from flask import abort
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
+from flask_login import current_user
+from jinja2.exceptions import TemplateNotFound
 from .. import app
 from .. import db
+from .. import login_manager
+from ..models import User
+from ..utils.security import is_safe_url
 from ..utils.security import crossdomain
 from ..utils.database import query
+from ..utils.database import parse_qstring
+from ..utils.database import query_names
 
-@app.route('/data/')
-@app.route('/data/<name>')
+login_manager.login_view = 'login'
+login_manager.login_message = 'Login required.'
+login_manager.login_message_category = "info"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Login user."""
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.verify_password(password):
+            login_user(user)
+            url = request.args.get('next')
+            if not is_safe_url(url):
+                return abort(400)
+            flash(u'Welcome {}'.format(username), 'success')
+            print('=============================================')
+            print(current_user.username)
+            return redirect(url or url_for('index'))
+        else:
+            flash(u'Invalid login, please try again', 'warning')
+    return render_template('login.html')
+
+@app.route('/')
+@login_required
+def index():
+    """Serve index page."""
+    context = dict(
+        private=query_names(app, 'private'),
+        public=query_names(app, 'public')
+    )
+    return render_template("index.html", **context)
+
+@app.route('/api/')
+def api():
+    return render_template('api.html')
+
+@app.route('/api/public')
+@app.route('/api/public/<name>')
 @crossdomain(origin=app.config.get('CROSSDOMAIN_ORIGIN', '*'))
-def data(name=None):
-    """Return dataset."""
-    ndays = 30
-    rel_path = os.path.join('templates', 'api', 'public')
-    templates = os.listdir(os.path.join(app.root_path, rel_path))
-    queries = [t[:-4] for t in templates if t.endswith('.sql')]
-    if not name:  # return list of queries
-        query_strings = ['days', 'from', 'to']
-        info = [
-            "syntax: /data/query_name?query_string",
-            "default date range is last {} days".format(ndays),
-            "specify a custom date range with 'days' or 'from' & 'to'",
-            "'days' accepts unsigned int",
-            "'from'/'to' accepts str of format YYYY-MM-DD",
-            "examples:",
-            "/data/visits?days=120",
-            "/data/platforms?from=2016-09-01&to=2017-02-20"
-            ]
-        return jsonify(queries=queries, query_strings=query_strings, info=info)
-    if name not in queries:  # query name does not exist
+def api_public(name=None):
+    if not name:
+        return api_collection('public')
+    return api_query('public', name, request)
+
+@app.route('/api/private')
+@app.route('/api/private/<name>')
+@crossdomain(origin=app.config.get('CROSSDOMAIN_ORIGIN', '*'))
+@login_required
+def api_private(name=None):
+    if not name:
+        return api_collection('private')
+    return api_query('private', name, request)
+
+def api_collection(collection):
+    queries = query_names(app, collection)
+    context = dict(
+        current_user=current_user,
+        collection=collection,
+        queries=queries
+        )
+    return render_template('api.html', **context)
+
+def api_query(collection, name, request):
+    success, context = parse_qstring(request)
+    if not success:
+        return jsonify(error=context)
+
+    file_path = os.path.join('api', collection, name + '.sql')
+    try:
+        sql = render_template(file_path, **context)
+    except TemplateNotFound as err:
         return jsonify(error="query '{}' does not exist.".format(name))
 
-    context = {
-        'days':request.args.get('days', type=int),
-        't0':request.args.get('from'),
-        't1':request.args.get('to'),
-    }
-    # validate and set time args
-    if all([context['t0'], context['t1'], context['days']]):  # bad args
-        return jsonify(error="Provided both from/to and days.")
-    elif context['t0'] and not context['t1']:  # bad args
-        return jsonify(error="Missing to date.")
-    elif context['t1'] and not context['t0']:  # bad args
-        return jsonify(error="Missing from date.")
-    elif all([context['t0'], context['t1']]):
-        strptime = datetime.datetime.strptime
-        try:
-            context['t0'] = strptime(context['t0'], '%Y-%m-%d')
-            context['t1'] = strptime(context['t1'], '%Y-%m-%d')
-        except ValueError as err:
-            return jsonify(error=err)
-    else:
-        today = datetime.datetime.utcnow().date()
-        if context['days']:
-            days = context['days']
-        else:
-            days = ndays
-        try:
-            context['t0'] = today - datetime.timedelta(days)
-        except ValueError as err:
-            return jsonify(error=err)
-        context['t1'] = today
+    return 	jsonify(query(db, sql))
 
-    # convert time args
-    if context['t0'] > context['t1']:
-        return jsonify(error="Time error: from > to.")
-    else:
-        context['t0'] = context['t0'].strftime('%Y-%m-%d')
-        context['t1'] = context['t1'].strftime('%Y-%m-%d')
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash(u'You have been logged out.', 'info')
+    return redirect(url_for('login'))
 
-    rel_path = os.path.join('api', 'public')
-    sql = render_template(os.path.join(rel_path, name + '.sql'), **context)
-    return 	jsonify(name=name, data=query(db, sql))
+@app.errorhandler(404)
+def not_found(error):
+    """Serve unknown route page."""
+    return 'This page does not exist', 404
