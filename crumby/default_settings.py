0a1ee3fdf15917ec8072e6930eac0da79e6a6a50
bmweiner <bmweiner@users.noreply.github.com>
2017-07-31 10:05:36 -0400

    Updated values.

--- crumby/default_settings.py
+++ crumby/default_settings.py
@@ -4,4 +4,6 @@ from os.path import join
 from os.path import expanduser
 
 DOMAIN = 'localhost:5000'
+GEOIP2_DATABASE_NAME = join(expanduser('~'), 'GeoLite2-City.mmdb')
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + join(expanduser('~'), 'crumby.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
